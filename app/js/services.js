8389b245a46e794ce41c4a506938eb904c0f2abb
tor <m.semkulich@internetdevels.com>
2016-10-06 10:14:21 +0300

    Refactored App.

--- app/js/services.js
+++ app/js/services.js
@@ -32,3 +32,39 @@ angular.module('openWeatherApp.services', ['ngResource'])
       }
     )
   });
+
+(function() {
+  'use strict';
+
+  angular
+    .module('openWeatherApp.services')
+    .service('openWeatherAPI', openWeatherAPI);
+
+  openWeatherAPI.$inject = ['$http', '$q'];
+
+  function openWeatherAPI($http, $q) {
+    var apiKey = '279b4be6d54c8bf6ea9b12275a567156';
+    var apiBaseUrl = 'http://api.openweathermap.org/data/2.5/weather?';
+
+    this.getDataByGeoLocation = function() {
+      var geoData = getGeoPosition();
+
+      return angular.isObject(geoData)
+        ? $http.get(apiBaseUrl, { params: { APPID: apiKey, lat: geoData.coords.latitude, lon: geoData.coords.longitude } })
+        : $q.defer().resolve(null);
+    };
+
+    this.getDataByCityName = function(name) {
+      return name
+        ? $http.get(apiBaseUrl, { params: { APPID: apiKey, q: name }})
+        : $q.defer().resolve(null);
+    };
+
+    function getGeoPosition() {
+      return navigator.geolocation
+        ? navigator.geolocation.getCurrentPosition(function(geoData) { return geoData; })
+        : null;
+    }
+  }
+})();
+
